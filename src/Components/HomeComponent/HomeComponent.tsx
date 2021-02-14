9ac40fe5c83f0da7882634e2b6c346fb18e105e0
anaviktoriv <anaviktoriv@gmail.com>
2021-02-14 17:14:56 +0100

    add breakpoints + font variables + style into mobile

--- src/Components/HomeComponent/HomeComponent.tsx
+++ src/Components/HomeComponent/HomeComponent.tsx
@@ -1,15 +1,33 @@
 import React, { FunctionComponent } from 'react';
+import { useTranslation } from 'react-i18next';
+
 import NavBar from '../../Components/NavBar/NavBar';
 import LanguageSelect from '../LanguageSelect/LanguageSelect';
+
+import './styles.scss';
 
 type HomeComponentProps = {};
 
 const HomeComponent: FunctionComponent<HomeComponentProps> = ({}) => {
+  const { t }: { t: any } = useTranslation();
+
   return (
     <>
       <NavBar activePage="aboutMe" />
       <LanguageSelect />
-      <div className="HomeComponentContainer"></div>
+      <div className="HomeComponentContainer">
+        <div className="HomeComponentContainer-IntroductionAndPitchContainer">
+          <h1 className="HomeComponentContainer-IntroductionAndPitchContainer-MainTitle">
+            {t('home.mainTitle')}
+          </h1>
+          <h2 className="HomeComponentContainer-IntroductionAndPitchContainer-Pitch">
+            {t('home.pitch')}
+          </h2>
+          <button className="HomeComponentContainer-IntroductionAndPitchContainer-DownloadButton">
+            {t('home.button.downloadResume')}
+          </button>
+        </div>
+      </div>
     </>
   );
 };
