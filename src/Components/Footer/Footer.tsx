72ed2b66e0cf681a8a7c9379b5d2997f4eb2ec70
anaviktoriv <anaviktoriv@gmail.com>
2021-03-10 14:37:35 +0100

    add footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,6 +2,11 @@ import React, { FunctionComponent } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
+import Phone from '../../assets/icons/phone_icon_white.svg';
+import Email from '../../assets/icons/email_icon_white.svg';
+import Github from '../../assets/icons/github_icon_white.svg';
+import Linkedin from '../../assets/icons/linkedin_icon_white.svg';
+
 import './styles.scss';
 
 type FooterProps = {};
@@ -12,10 +17,68 @@ const Footer: FunctionComponent<FooterProps> = ({}) => {
   return (
     <footer className="FooterContainer">
       <div className="FooterContainer-PolicyContainer">
-        <h5>&#169; Anastasia Tanczak</h5>
+        <p className="FooterContainer-PolicyContainer-CopyRight">
+          &#169; Anastasia Tanczak
+        </p>
+        <Link
+          className="FooterContainer-PolicyContainer-PrivacyPolicyLink"
+          to="/privacypolicy"
+        >
+          {t('footer.privacyPolicy')}
+        </Link>
       </div>
-      <div className="FooterContainer-ContactContainer"></div>
-      <div className="FooterContainer-SocialLinksContainer"></div>
+      <div className="FooterContainer-ContactContainer">
+        <h5 className="FooterContainer-ContactContainer-Title">
+          {t('footer.contact')}
+        </h5>
+        <div className="FooterContainer-ContactContainer-Contact">
+          <img
+            className="FooterContainer-ContactContainer-Contact-Icon"
+            src={Phone}
+            alt="phone icon"
+          />
+          <p className="FooterContainer-ContactContainer-Contact-Text">
+            +33(0)6 95 56 49 93
+          </p>
+        </div>
+        <div className="FooterContainer-ContactContainer-Contact">
+          <img
+            className="FooterContainer-ContactContainer-Contact-Icon"
+            src={Email}
+            alt="Email icon"
+          />
+          <p className="FooterContainer-ContactContainer-Contact-Text">
+            anatkachen@gmail.com
+          </p>
+        </div>
+      </div>
+      <div className="FooterContainer-SocialLinksContainer">
+        <h5 className="FooterContainer-SocialLinksContainer-Title">
+          {t('footer.followMe')}
+        </h5>
+        <div className="FooterContainer-SocialLinksContainer-Links">
+          <a
+            className="FooterContainer-SocialLinksContainer-Links-Link"
+            href="https://www.linkedin.com/in/anastasiatanczak/"
+          >
+            <img
+              className="FooterContainer-SocialLinksContainer-Links-Link-Icon"
+              src={Linkedin}
+              alt="linkedin icon"
+            />
+          </a>
+          <a
+            className="FooterContainer-SocialLinksContainer-Links-Link"
+            href="https://github.com/anaviktoriv"
+          >
+            <img
+              className="FooterContainer-SocialLinksContainer-Links-Link-Icon"
+              src={Github}
+              alt="github icon"
+            />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
